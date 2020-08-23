480733945909676e7c0a22074cae4f678f9c1dca
Matthew <matthewlee031205@gmail.com>
2020-08-22 23:11:29 -0400

    Update Reader.py

--- Reader.py
+++ Reader.py
@@ -2,8 +2,13 @@ import csv
 alist = list()
 file = "file name here"
 
-with open(file, 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    for i in csv_reader:
-        alist.append(i)
 
+def file():
+    with open(file, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for i in csv_reader:
+            alist.append(i)
+
+if __name__ == "__main__":
+    file()
+    print("CSV outputted")
